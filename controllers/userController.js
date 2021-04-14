c94cf5749fe92568059df946a0332db06dd055dd
Darwin <id.darwins@gmail.com>
2021-04-15 01:08:29 +0700

    Adding base : index >> route >> controllers. MongoDB connected. npm (dotenv, express, mongoose, validator).

---
+++ controllers/userController.js
@@ -0,0 +1,5 @@
+const { user, movie, review, caster } = require("../models");
+
+class UserController {}
+
+module.exports = new UserController();
---
+++ routes/movieRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+// Import validator
+
+// Import controller
+const movieController = require("../controllers/movieController");
+
+// Import auth (middleware)
+
+// Make router
+const router = express.Router();
+
+// Exports router
+module.exports = router;
