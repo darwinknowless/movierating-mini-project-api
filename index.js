8d1ba7cce6d743a6fb6df744df5ed3f540c767df
Darwin <id.darwins@gmail.com>
2021-04-19 13:26:48 +0700

    update data

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 // Express
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const fileUpload = require("express-fileupload"); // Import express-fileupload
 
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+
 // Import Router
 const reviewRoutes = require("./routes/reviewRoutes");
-// const movieRoutes = require("./routes/movieRoutes");
-// const casterRoutes = require("./routes/casterRoutes");
+const movieRoutes = require("./routes/movieRoutes");
+const casterRoutes = require("./routes/casterRoutes");
 const userRoutes = require("./routes/userRoutes");
-// const categoryRoutes = require("./routes/categoryRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 
 // Make app
 const app = express();
@@ -25,16 +35,55 @@ app.use(
 
 // To read form-data
 app.use(fileUpload());
+// Sanitize data
+app.use(mongoSanitize());
+// Prevent XSS attact
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 mins
+  max: 100,
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+// Use helmet
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+// CORS
+app.use(cors());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+} else {
+  // create a write stream (in append mode)
+  let accessLogStream = fs.createWriteStream(
+    path.join(__dirname, "access.log"),
+    {
+      flags: "a",
+    }
+  );
+
+  // setup the logger
+  app.use(morgan("combined", { stream: accessLogStream }));
+}
 
 // Static folder (for image)
 app.use(express.static("public"));
 
 // Make routes
 app.use("/review", reviewRoutes);
-// app.use("/movie", movieRoutes);
-// app.use("/caster", casterRoutes);
+app.use("/movie", movieRoutes);
+app.use("/caster", casterRoutes);
 app.use("/user", userRoutes);
-// app.use("/category", categoryRoutes);
+app.use("/category", categoryRoutes);
 
 // If environment is not test
 if (process.env.NODE_ENV !== "test") {
